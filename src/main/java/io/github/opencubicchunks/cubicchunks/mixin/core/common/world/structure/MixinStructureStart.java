5e520f93b5119eb1731043ed607ea7b93ed76053
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2021-02-19 20:59:28 -0800

    Locate pos Y fix

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/world/structure/MixinStructureStart.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/world/structure/MixinStructureStart.java
@@ -16,6 +16,8 @@ import net.minecraft.world.level.levelgen.structure.StructureStart;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(StructureStart.class)
 public abstract class MixinStructureStart implements SetupCubeStructureStart {
@@ -24,6 +26,8 @@ public abstract class MixinStructureStart implements SetupCubeStructureStart {
     @Shadow @Final protected List<StructurePiece> pieces;
 
     @Shadow protected abstract void calculateBoundingBox();
+
+    @Shadow protected BoundingBox boundingBox;
 
     @Override
     public void placeInCube(WorldGenLevel worldGenLevel, StructureFeatureManager structureFeatureManager, ChunkGenerator chunkGenerator, Random random, BoundingBox boundingBox,
@@ -47,5 +51,10 @@ public abstract class MixinStructureStart implements SetupCubeStructureStart {
                 this.calculateBoundingBox();
             }
         }
+    }
+
+    @ModifyArg(method = "getLocatePos", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;<init>(III)V"), index = 1)
+    private int getStructureY(int arg0) {
+        return (this.boundingBox.y0 + this.boundingBox.y1) >> 1;
     }
 }
