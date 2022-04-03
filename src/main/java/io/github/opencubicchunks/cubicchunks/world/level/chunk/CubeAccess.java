7dae49176f171178bdaedea0b4c546b88961e825
BelgianSalamander <salamanderhash0514@gmail.com>
2022-04-04 10:37:52 +1200

    Fix usages of getSection() in ChunkAccess

--- src/main/java/io/github/opencubicchunks/cubicchunks/world/level/chunk/CubeAccess.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/world/level/chunk/CubeAccess.java
@@ -13,11 +13,17 @@ import io.github.opencubicchunks.cc_core.world.heightmap.HeightmapSource;
 import io.github.opencubicchunks.cc_core.world.heightmap.surfacetrackertree.SurfaceTrackerLeaf;
 import it.unimi.dsi.fastutil.shorts.ShortList;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.QuartPos;
 import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelHeightAccessor;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeResolver;
+import net.minecraft.world.level.biome.Climate;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
@@ -185,6 +191,71 @@ public abstract class CubeAccess extends ChunkAccess implements BlockGetter, Fea
 
     public Map<Heightmap.Types, SurfaceTrackerLeaf[]> getCubeHeightmaps() {
         return cubeHeightmaps;
+    }
+
+    //Fix usages of getSectionIndex()
+    @Override
+    public int getSectionIndex(int i) {
+        throw new UnsupportedOperationException("Sections are got from (x, y, z) in cube not just y");
+    }
+
+    @Override
+    public boolean isYSpaceEmpty(int minY, int maxY) {
+        if (minY < this.getMinBuildHeight()) {
+            minY = this.getMinBuildHeight();
+        }
+
+        if (maxY > this.getMaxBuildHeight()) {
+            maxY = this.getMaxBuildHeight();
+        }
+
+        for (int y = minY; y <= maxY; y += 16) {
+            for (int x = 0; x < DIAMETER_IN_BLOCKS; x += LevelChunkSection.SECTION_WIDTH) {
+                for (int z = 0; z < DIAMETER_IN_BLOCKS; z += LevelChunkSection.SECTION_WIDTH) {
+                    LevelChunkSection section = this.getSection(Coords.blockToIndex(x, y, z));
+
+                    if (!section.hasOnlyAir()) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    //TODO: Maybe use mixin to prevent method duplication
+    @Override
+    public Holder<Biome> getNoiseBiome(int x, int y, int z) {
+        try {
+            int clampedX = Mth.clamp(x, cubePos.minCubeX(), cubePos.maxCubeX());
+            int clampedY = Mth.clamp(y, cubePos.minCubeY(), cubePos.maxCubeY());
+            int clampedZ = Mth.clamp(z, cubePos.minCubeZ(), cubePos.maxCubeZ());
+
+            return this.sections[Coords.blockToIndex(clampedX, clampedY, clampedZ)].getNoiseBiome(clampedX & 0x03, clampedY & 0x03, clampedZ & 0x03);
+        } catch (Throwable e) {
+            CrashReport crashReport = CrashReport.forThrowable(e, "Getting biome");
+            CrashReportCategory crashReportCategory = crashReport.addCategory("Biome being got");
+            crashReportCategory.setDetail("Location", () -> CrashReportCategory.formatLocation(this, x, y, z));
+            throw new ReportedException(crashReport);
+        }
+    }
+
+    @Override
+    public void fillBiomesFromNoise(BiomeResolver biomeResolver, Climate.Sampler sampler) {
+        LevelHeightAccessor heightAccessor = this.getHeightAccessorForGeneration();
+
+        for (int sectionY = heightAccessor.getMinSection(); sectionY < heightAccessor.getMaxSection(); sectionY++) {
+            for (int sectionX = 0; sectionX < DIAMETER_IN_SECTIONS; sectionX++) {
+                for (int sectionZ = 0; sectionZ < DIAMETER_IN_SECTIONS; sectionZ++) {
+                    int minXQuart = QuartPos.fromBlock(Coords.sectionToMinBlock(sectionX));
+                    int minZQuart = QuartPos.fromBlock(Coords.sectionToMinBlock(sectionZ));
+
+                    LevelChunkSection section = this.sections[Coords.sectionToIndex(sectionX, sectionY, sectionZ)];
+                    section.fillBiomesFromNoise(biomeResolver, sampler, minXQuart, minZQuart);
+                }
+            }
+        }
     }
 
     //TODO: Implement other methods
