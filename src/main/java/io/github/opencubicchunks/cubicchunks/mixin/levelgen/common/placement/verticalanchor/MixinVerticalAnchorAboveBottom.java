4ad6ca26b2e1f14216f8e090dc952aedd5371652
Tom Martin <tom.martin1239@gmail.com>
2022-08-24 22:04:43 +0100

    Shadow field instead of doing unnecessary unsafeCast

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/levelgen/common/placement/verticalanchor/MixinVerticalAnchorAboveBottom.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/levelgen/common/placement/verticalanchor/MixinVerticalAnchorAboveBottom.java
@@ -1,10 +1,11 @@
 package io.github.opencubicchunks.cubicchunks.mixin.levelgen.common.placement.verticalanchor;
 
 import io.github.opencubicchunks.cubicchunks.levelgen.carver.CubicCarvingContext;
-import io.github.opencubicchunks.cubicchunks.utils.Utils;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.WorldGenerationContext;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -12,15 +13,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(VerticalAnchor.AboveBottom.class)
 public class MixinVerticalAnchorAboveBottom {
 
+    @Shadow @Final private int offset;
+
     @Inject(method = "resolveY", at = @At("HEAD"), cancellable = true)
     private void resolveCubicChunksY(WorldGenerationContext context, CallbackInfoReturnable<Integer> cir) {
         if (context instanceof CubicCarvingContext) {
-            int defaultValue = ((CubicCarvingContext) context).getOriginalMinGenY() + getThis().offset();
+            int defaultValue = ((CubicCarvingContext) context).getOriginalMinGenY() + offset;
             cir.setReturnValue(defaultValue);
         }
-    }
-
-    private VerticalAnchor.AboveBottom getThis() {
-        return Utils.unsafeCast(this);
     }
 }
--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/levelgen/common/placement/verticalanchor/MixinVerticalAnchorBelowTop.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/levelgen/common/placement/verticalanchor/MixinVerticalAnchorBelowTop.java
@@ -1,25 +1,24 @@
 package io.github.opencubicchunks.cubicchunks.mixin.levelgen.common.placement.verticalanchor;
 
 import io.github.opencubicchunks.cubicchunks.levelgen.carver.CubicCarvingContext;
-import io.github.opencubicchunks.cubicchunks.utils.Utils;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.WorldGenerationContext;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(VerticalAnchor.BelowTop.class)
 public class MixinVerticalAnchorBelowTop {
+    @Shadow @Final private int offset;
+
     @Inject(method = "resolveY", at = @At("HEAD"), cancellable = true)
     private void resolveCubicChunksY(WorldGenerationContext context, CallbackInfoReturnable<Integer> cir) {
         if (context instanceof CubicCarvingContext) {
-            int defaultValue = ((CubicCarvingContext) context).getOriginalGenDepth() - 1 + ((CubicCarvingContext) context).getOriginalMinGenY() - getThis().offset();
+            int defaultValue = ((CubicCarvingContext) context).getOriginalGenDepth() - 1 + ((CubicCarvingContext) context).getOriginalMinGenY() - offset;
             cir.setReturnValue(defaultValue);
         }
-    }
-
-    private VerticalAnchor.AboveBottom getThis() {
-        return Utils.unsafeCast(this);
     }
 }
