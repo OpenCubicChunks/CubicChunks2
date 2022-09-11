8b1852577159948854abf3e0e11c6a7cf30e56c9
Bartosz Skrzypczak <Barteks2x@gmail.com>
2022-09-11 02:11:33 +0200

    Fix chunks not loading around player

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/asmfixes/common/ticket/DistanceManagerFixes.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/asmfixes/common/ticket/DistanceManagerFixes.java
@@ -1,8 +1,10 @@
 package io.github.opencubicchunks.cubicchunks.mixin.asmfixes.common.ticket;
 
 import io.github.opencubicchunks.cc_core.api.CubePos;
+import io.github.opencubicchunks.cubicchunks.server.level.CubicTicketType;
 import net.minecraft.core.SectionPos;
 import net.minecraft.server.level.DistanceManager;
+import net.minecraft.server.level.TicketType;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,5 +16,14 @@ public class DistanceManagerFixes {
         at = @At(value = "INVOKE", target = "Lnet/minecraft/core/SectionPos;cube__ReplaceWithMixin()Lio/github/opencubicchunks/cc_core/api/CubePos;"))
     private CubePos sectionPosToCubePos(SectionPos sectionPos) {
         return CubePos.from(sectionPos);
+    }
+
+    @Dynamic @Redirect(
+        method = { "dasm$redirect$lambda$runAllUpdates$1", "dasm$redirect$method_14040" },
+        at = @At(value = "FIELD", target = "Lnet/minecraft/server/level/TicketType;PLAYER:Lnet/minecraft/server/level/TicketType;"),
+        require = 1
+    )
+    private static TicketType<?> getPlayerTicketTypeCC() {
+        return CubicTicketType.PLAYER;
     }
 }
