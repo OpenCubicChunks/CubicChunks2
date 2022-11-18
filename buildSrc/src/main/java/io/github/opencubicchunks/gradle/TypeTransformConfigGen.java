bd522addb25749e52cc37bb2249e5e0fc81bfecb
BelgianSalamander <salamanderhash0514@gmail.com>
2022-11-18 16:18:01 +1300

    Get type info at compile time

--- buildSrc/src/main/java/io/github/opencubicchunks/gradle/TypeTransformConfigGen.java
+++ buildSrc/src/main/java/io/github/opencubicchunks/gradle/TypeTransformConfigGen.java
@@ -1,9 +1,21 @@
 package io.github.opencubicchunks.gradle;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringWriter;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -17,7 +29,17 @@ import net.fabricmc.mappingio.tree.MappingTree;
 import net.fabricmc.mappingio.tree.MappingTreeView;
 import net.fabricmc.mappingio.tree.MemoryMappingTree;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.PublishArtifact;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Handle;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.ClassNode;
 
 public class TypeTransformConfigGen {
     private static final Gson GSON = new Gson();
@@ -31,9 +53,16 @@ public class TypeTransformConfigGen {
     private final int toIdx;
     private final int fromIdx;
 
-    private TypeTransformConfigGen(MappingsProviderImpl mappingsProvider, String content) throws IOException {
+    private final Project project;
+
+    private final Set<ZipFile> jars = new HashSet<>();
+
+    private final Map<String, ClassNode> classCache = new HashMap<>();
+
+    private TypeTransformConfigGen(Project project, MappingsProviderImpl mappingsProvider, String content) throws IOException {
         this.config = GSON.fromJson(content, JsonElement.class);
         this.mappings = mappingsProvider.getMappings();
+        this.project = project;
 
         this.fromIdx = this.mappings.getDstNamespaces().indexOf(MAP_FROM);
         this.toIdx = this.mappings.getDstNamespaces().indexOf(MAP_TO);
@@ -45,26 +74,27 @@ public class TypeTransformConfigGen {
         if (toIdx == -1 && !MAP_TO.equals(this.mappings.getSrcNamespace())) {
             throw new IllegalStateException("Cannot find namespace " + MAP_TO + " in mappings");
         }
-    }
 
-    public static String apply(Project project, String content) throws IOException {
-        System.out.println("Amending type transform config");
-        LoomGradleExtension loom = (LoomGradleExtension) project.getExtensions().getByName("loom");
 
-        MappingsProviderImpl mappingsProvider = loom.getMappingsProvider();
-        System.out.println("mappingsProvider.getMappingsName() = " + mappingsProvider.mappingsIdentifier);
-        MemoryMappingTree mappings = mappingsProvider.getMappings();
+        for (Configuration config: project.getConfigurations()) {
+            if (config.getName().startsWith("runtime") && config.isCanBeResolved()) {
+                for (File file : config.resolve()) {
+                    if (file.getName().endsWith(".jar")) {
+                        jars.add(new ZipFile(file));
+                    }
+                }
+            }
+        }
 
-        System.out.println(mappings.getDstNamespaces());
-        System.out.println(mappings.getSrcNamespace());
-
-        TypeTransformConfigGen gen = new TypeTransformConfigGen(mappingsProvider, content);
-
-        return gen.generate();
+        for (ZipFile jar : jars) {
+            System.out.println("Loading " + jar.getName());
+        }
     }
 
     private String generate() {
         JsonObject root = config.getAsJsonObject();
+        this.generateTypeInfo(root);
+
         if (root.has("invokers")) {
             this.processInvokerData(root.getAsJsonArray("invokers"));
         }
@@ -79,6 +109,144 @@ public class TypeTransformConfigGen {
         GSON.toJson(res, jsonWriter);
 
         return writer.toString();
+    }
+
+    private void generateTypeInfo(JsonObject root) {
+        JsonArray typeInfo = new JsonArray();
+        Set<Type> processed = new HashSet<>();
+        Queue<Type> queue = new ArrayDeque<>();
+
+        JsonObject meta = root.getAsJsonObject("type_meta_info");
+        JsonArray inspect = meta.getAsJsonArray("inspect");
+
+        for (JsonElement e : inspect) {
+            ClassNode classNode = findClass(e.getAsString());
+            queue.addAll(getAllUsedTypes(classNode));
+        }
+
+        while (!queue.isEmpty()) {
+            Type top = queue.poll();
+
+            if (processed.contains(top)) continue;
+            processed.add(top);
+
+            JsonObject typeInfoEntry = new JsonObject();
+            ClassNode classNode = findClass(top.getInternalName());
+
+            typeInfoEntry.addProperty("name", top.getClassName().replace('.', '/'));
+            typeInfoEntry.addProperty("is_interface", (classNode.access & Opcodes.ACC_INTERFACE) != 0);
+
+            typeInfoEntry.addProperty("superclass", classNode.superName);
+            if (classNode.superName != null) {
+                queue.add(Type.getObjectType(classNode.superName));
+            }
+
+            JsonArray interfaces = new JsonArray();
+            for (String iface : classNode.interfaces) {
+                interfaces.add(iface);
+                queue.add(Type.getObjectType(iface));
+            }
+            typeInfoEntry.add("interfaces", interfaces);
+
+            typeInfo.add(typeInfoEntry);
+        }
+
+        meta.add("type_info", typeInfo);
+    }
+
+    private Set<Type> getAllUsedTypes(ClassNode node) {
+        Set<Type> types = new HashSet<>();
+        types.add(Type.getObjectType(node.name));
+
+        ClassVisitor visitor = new ClassVisitor(Opcodes.ASM9) {
+            @Override public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
+                types.add(Type.getType(descriptor));
+                return null;
+            }
+
+            @Override public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+                Type[] args = Type.getArgumentTypes(descriptor);
+                types.addAll(Arrays.asList(args));
+                types.add(Type.getReturnType(descriptor));
+                return new MethodVisitor(Opcodes.ASM9) {
+                    @Override public void visitTypeInsn(int opcode, String type) {
+                        types.add(Type.getObjectType(type));
+                    }
+
+                    @Override public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
+                        types.add(Type.getObjectType(owner));
+                        types.add(Type.getType(descriptor));
+                    }
+
+                    @Override public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
+                        types.add(Type.getObjectType(owner));
+                        Type[] args = Type.getArgumentTypes(descriptor);
+                        types.addAll(Arrays.asList(args));
+                        types.add(Type.getReturnType(descriptor));
+                    }
+
+                    @Override public void visitInvokeDynamicInsn(String name, String descriptor, Handle bootstrapMethodHandle, Object... bootstrapMethodArguments) {
+                        //TODO: Do this better
+                        types.add(Type.getReturnType(descriptor));
+                    }
+
+                    @Override public void visitMultiANewArrayInsn(String descriptor, int numDimensions) {
+                        types.add(Type.getType(descriptor));
+                    }
+                };
+            }
+        };
+
+        node.accept(visitor);
+
+        return types.stream()
+            .map(t -> t.getSort() == Type.ARRAY ? t.getElementType() : t)
+            .filter(t -> t.getSort() == Type.OBJECT)
+            .collect(Collectors.toSet());
+    }
+
+    private ClassNode findClass(String name) {
+        if (classCache.containsKey(name)) {
+            return classCache.get(name);
+        }
+
+        String path = name.replace('.', '/') + ".class";
+
+        InputStream in = null;
+        for (ZipFile jar : jars) {
+            ZipEntry entry = jar.getEntry(path);
+            if (entry != null) {
+                try {
+                    in = jar.getInputStream(entry);
+                    break;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        if (in == null) {
+            try {
+                in = ClassLoader.getSystemResourceAsStream(path);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (in == null) {
+            throw new IllegalStateException("Cannot find class " + name);
+        }
+
+        try {
+            ClassReader reader = new ClassReader(in);
+            ClassNode node = new ClassNode();
+            reader.accept(node, 0);
+            classCache.put(name, node);
+            System.out.println("Found class " + name);
+            return node;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void processInvokerData(JsonArray element) {
@@ -210,5 +378,21 @@ public class TypeTransformConfigGen {
         } else {
             return element.getDstName(this.toIdx);
         }
+    }
+
+    public static String apply(Project project, String content) throws IOException {
+        System.out.println("Amending type transform config");
+        LoomGradleExtension loom = (LoomGradleExtension) project.getExtensions().getByName("loom");
+
+        MappingsProviderImpl mappingsProvider = loom.getMappingsProvider();
+        System.out.println("mappingsProvider.getMappingsName() = " + mappingsProvider.mappingsIdentifier);
+        MemoryMappingTree mappings = mappingsProvider.getMappings();
+
+        System.out.println(mappings.getDstNamespaces());
+        System.out.println(mappings.getSrcNamespace());
+
+        TypeTransformConfigGen gen = new TypeTransformConfigGen(project, mappingsProvider, content);
+
+        return gen.generate();
     }
 }
