da59fedca194648085bdd2fd749984e6dcf58c26
Tom Martin <tom.martin1239@gmail.com>
2023-04-17 02:58:31 +0100

    Add additional simple light engine tests

--- src/test/java/io/github/opencubicchunks/cubicchunks/levelgen/lighting/LightingTests.java
+++ src/test/java/io/github/opencubicchunks/cubicchunks/levelgen/lighting/LightingTests.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 import java.util.Set;
 
 import io.github.opencubicchunks.cc_core.api.CubePos;
+import io.github.opencubicchunks.cc_core.api.CubicConstants;
 import io.github.opencubicchunks.cc_core.world.ColumnCubeMap;
 import io.github.opencubicchunks.cubicchunks.mock.TestBlockGetter;
 import io.github.opencubicchunks.cubicchunks.mock.interfaces.BlockGetterLightHeightmapGetterColumnCubeMapGetter;
@@ -103,6 +104,144 @@ public class LightingTests {
         levelLightEngine.enableLightSources(new ChunkPos(0, 0), true);
 
         ((CubicSkyLightEngine) (Object) levelLightEngine).doSkyLightForCube(cubeAccess);
+
+        validateSkyLighting(levelLightEngine, blockGetter, Set.of(SectionPos.of(0, 0, 0)), blockGetter.getHeightmap().inner);
+    }
+
+    @Test
+    public void testSkyLightEngineExistingTerrain() {
+        TestBlockGetter blockGetter = new TestBlockGetter();
+
+        for (int x = 0; x < SectionPos.SECTION_SIZE; x++) {
+            for (int z = 0; z < SectionPos.SECTION_SIZE; z++) {
+                for (int y = 0; y < SectionPos.SECTION_SIZE; y++) {
+                    blockGetter.setBlockState(new BlockPos(x, y, z), Math.max(x, z) > y ? Blocks.STONE.defaultBlockState() : Blocks.AIR.defaultBlockState());
+                }
+            }
+        }
+
+        LightCubeChunkGetter lightCubeGetter = mock(LightCubeChunkGetter.class);
+        when(lightCubeGetter.getLevel()).thenReturn(blockGetter);
+
+        ColumnCubeMap columnCubeMap = new ColumnCubeMap();
+        columnCubeMap.markLoaded(0);
+
+        BlockGetterLightHeightmapGetterColumnCubeMapGetter lightHeightmapGetter = mock(BlockGetterLightHeightmapGetterColumnCubeMapGetter.class);
+        when(lightHeightmapGetter.getLightHeightmap()).thenReturn(blockGetter.getHeightmap());
+        when(lightHeightmapGetter.getCubeMap()).thenReturn(columnCubeMap);
+
+        when(lightCubeGetter.getChunkForLighting(anyInt(), anyInt())).thenReturn(lightHeightmapGetter);
+
+        CubeAccess cubeAccess = mock(CubeAccess.class);
+        when(cubeAccess.getCubePos()).thenReturn(CubePos.of(0, 0, 0));
+        when(cubeAccess.getStatus()).thenReturn(ChunkStatus.LIGHT);
+
+        when(lightCubeGetter.getCubeForLighting(0, 0, 0)).thenReturn(blockGetter);
+
+        SkyLightEngine levelLightEngine = new SkyLightEngine(lightCubeGetter);
+        ((CubicLayerLightEngine) (Object) levelLightEngine).setCubic();
+
+        levelLightEngine.updateSectionStatus(SectionPos.of(0, 0, 0), false);
+        levelLightEngine.enableLightSources(new ChunkPos(0, 0), true);
+
+        ((CubicSkyLightEngine) (Object) levelLightEngine).doSkyLightForCube(cubeAccess);
+
+        validateSkyLighting(levelLightEngine, blockGetter, Set.of(SectionPos.of(0, 0, 0)), blockGetter.getHeightmap().inner);
+    }
+
+    @Test
+    public void testSkyLightEngineFullCover() {
+        TestBlockGetter blockGetter = new TestBlockGetter();
+
+        for (int x = 0; x < SectionPos.SECTION_SIZE; x++) {
+            for (int z = 0; z < SectionPos.SECTION_SIZE; z++) {
+                for (int y = 0; y < SectionPos.SECTION_SIZE; y++) {
+                    blockGetter.setBlockState(new BlockPos(x, y, z), Blocks.AIR.defaultBlockState());
+                }
+            }
+        }
+
+        for (int x = 0; x < SectionPos.SECTION_SIZE; x++) {
+            for (int z = 0; z < SectionPos.SECTION_SIZE; z++) {
+                blockGetter.setBlockState(new BlockPos(x, 3, z), Blocks.STONE.defaultBlockState());
+            }
+        }
+
+        LightCubeChunkGetter lightCubeGetter = mock(LightCubeChunkGetter.class);
+        when(lightCubeGetter.getLevel()).thenReturn(blockGetter);
+
+        ColumnCubeMap columnCubeMap = new ColumnCubeMap();
+        columnCubeMap.markLoaded(0);
+
+        BlockGetterLightHeightmapGetterColumnCubeMapGetter lightHeightmapGetter = mock(BlockGetterLightHeightmapGetterColumnCubeMapGetter.class);
+        when(lightHeightmapGetter.getLightHeightmap()).thenReturn(blockGetter.getHeightmap());
+        when(lightHeightmapGetter.getCubeMap()).thenReturn(columnCubeMap);
+
+        when(lightCubeGetter.getChunkForLighting(anyInt(), anyInt())).thenReturn(lightHeightmapGetter);
+
+        CubeAccess cubeAccess = mock(CubeAccess.class);
+        when(cubeAccess.getCubePos()).thenReturn(CubePos.of(0, 0, 0));
+        when(cubeAccess.getStatus()).thenReturn(ChunkStatus.LIGHT);
+
+        when(lightCubeGetter.getCubeForLighting(0, 0, 0)).thenReturn(blockGetter);
+
+        SkyLightEngine levelLightEngine = new SkyLightEngine(lightCubeGetter);
+        ((CubicLayerLightEngine) (Object) levelLightEngine).setCubic();
+
+        levelLightEngine.updateSectionStatus(SectionPos.of(0, 0, 0), false);
+        levelLightEngine.enableLightSources(new ChunkPos(0, 0), true);
+
+        ((CubicSkyLightEngine) (Object) levelLightEngine).doSkyLightForCube(cubeAccess);
+
+        validateSkyLighting(levelLightEngine, blockGetter, Set.of(SectionPos.of(0, 0, 0)), blockGetter.getHeightmap().inner);
+    }
+
+    @Test
+    public void testSkyLightEngineFullCoverWithHole() {
+        TestBlockGetter blockGetter = new TestBlockGetter();
+
+        for (int x = 0; x < SectionPos.SECTION_SIZE; x++) {
+            for (int z = 0; z < SectionPos.SECTION_SIZE; z++) {
+                for (int y = 0; y < SectionPos.SECTION_SIZE; y++) {
+                    blockGetter.setBlockState(new BlockPos(x, y, z), Blocks.AIR.defaultBlockState());
+                }
+            }
+        }
+
+        for (int x = 0; x < CubicConstants.DIAMETER_IN_BLOCKS; x++) {
+            for (int z = 0; z < CubicConstants.DIAMETER_IN_BLOCKS; z++) {
+                blockGetter.setBlockState(new BlockPos(x, 3, z), Blocks.STONE.defaultBlockState());
+            }
+        }
+
+        blockGetter.setBlockState(new BlockPos(7, 3, 7), Blocks.AIR.defaultBlockState());
+
+        LightCubeChunkGetter lightCubeGetter = mock(LightCubeChunkGetter.class);
+        when(lightCubeGetter.getLevel()).thenReturn(blockGetter);
+
+        ColumnCubeMap columnCubeMap = new ColumnCubeMap();
+        columnCubeMap.markLoaded(0);
+
+        BlockGetterLightHeightmapGetterColumnCubeMapGetter lightHeightmapGetter = mock(BlockGetterLightHeightmapGetterColumnCubeMapGetter.class);
+        when(lightHeightmapGetter.getLightHeightmap()).thenReturn(blockGetter.getHeightmap());
+        when(lightHeightmapGetter.getCubeMap()).thenReturn(columnCubeMap);
+
+        when(lightCubeGetter.getChunkForLighting(anyInt(), anyInt())).thenReturn(lightHeightmapGetter);
+
+        CubeAccess cubeAccess = mock(CubeAccess.class);
+        when(cubeAccess.getCubePos()).thenReturn(CubePos.of(0, 0, 0));
+        when(cubeAccess.getStatus()).thenReturn(ChunkStatus.LIGHT);
+
+        when(lightCubeGetter.getCubeForLighting(0, 0, 0)).thenReturn(blockGetter);
+
+        SkyLightEngine levelLightEngine = new SkyLightEngine(lightCubeGetter);
+        ((CubicLayerLightEngine) (Object) levelLightEngine).setCubic();
+
+        levelLightEngine.updateSectionStatus(SectionPos.of(0, 0, 0), false);
+        levelLightEngine.enableLightSources(new ChunkPos(0, 0), true);
+
+        ((CubicSkyLightEngine) (Object) levelLightEngine).doSkyLightForCube(cubeAccess);
+        levelLightEngine.runUpdates(Integer.MAX_VALUE, true, true);
 
         validateSkyLighting(levelLightEngine, blockGetter, Set.of(SectionPos.of(0, 0, 0)), blockGetter.getHeightmap().inner);
     }
