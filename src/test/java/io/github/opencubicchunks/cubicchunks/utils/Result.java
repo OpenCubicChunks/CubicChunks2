6becbcca204357a21c36af51d240767e9a22c053
Tom Martin <tom.martin1239@gmail.com>
2023-04-18 18:10:39 +0100

    Add more utility functions to Result

--- src/test/java/io/github/opencubicchunks/cubicchunks/utils/Result.java
+++ src/test/java/io/github/opencubicchunks/cubicchunks/utils/Result.java
@@ -1,5 +1,10 @@
 package io.github.opencubicchunks.cubicchunks.utils;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import org.jetbrains.annotations.Nullable;
+
 public abstract class Result<T, E> {
     private Result() {
     }
@@ -7,18 +12,23 @@ public abstract class Result<T, E> {
     public static <T, E> Ok<T, E> ok(final T value) {
         return new Ok<>(value);
     }
-
     public static <T, E> Err<T, E> err(final E value) {
         return new Err<>(value);
     }
 
     public abstract boolean isOk();
-
     public abstract boolean isErr();
 
     public abstract T asOk();
-
     public abstract E asErr();
+
+    public abstract void ifOk(Consumer<T> consumer);
+    public abstract void ifErr(Consumer<E> consumer);
+
+    @Nullable public abstract <R> R mapOk(Function<T, R> f);
+    @Nullable public abstract <R> R mapErr(Function<E, R> f);
+
+    public abstract <R> R map(Function<T, R> okF, Function<E, R> errF);
 
     private static final class Ok<T, E> extends Result<T, E> {
         private final T value;
@@ -45,6 +55,22 @@ public abstract class Result<T, E> {
         @Override
         public E asErr() {
             return null;
+        }
+
+        public void ifOk(Consumer<T> consumer) {
+            consumer.accept(this.value);
+        }
+        public void ifErr(Consumer<E> consumer) { }
+
+        @Nullable public <R> R mapOk(Function<T, R> f) {
+            return f.apply(this.value);
+        }
+        @Nullable public <R> R mapErr(Function<E, R> f) {
+            return null;
+        }
+
+        public <R> R map(Function<T, R> okF, Function<E, R> errF) {
+            return okF.apply(this.value);
         }
     }
 
@@ -73,6 +99,22 @@ public abstract class Result<T, E> {
         @Override
         public E asErr() {
             return value;
+        }
+
+        public void ifOk(Consumer<T> consumer) {}
+        public void ifErr(Consumer<E> consumer) {
+            consumer.accept(this.value);
+        }
+
+        @Nullable public <R> R mapOk(Function<T, R> f) {
+            return null;
+        }
+        @Nullable public <R> R mapErr(Function<E, R> f) {
+            return f.apply(this.value);
+        }
+
+        public <R> R map(Function<T, R> okF, Function<E, R> errF) {
+            return errF.apply(this.value);
         }
     }
 }
