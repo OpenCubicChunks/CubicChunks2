660f14a7976b0cce33b0e204cab99fae9a697ca9
Bartosz Skrzypczak <barteks2x@gmail.com>
2023-06-26 01:10:00 +1200

    Bump dasm and javaheaders version to get around jitpack issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     id("maven-publish")
     id("checkstyle")
     id("io.github.juuxel.loom-quiltflower").version("1.7.2")
-    id("io.github.opencubicchunks.javaheaders").version("1.2.5")
+    id("io.github.opencubicchunks.javaheaders").version("1.2.7")
     id("io.github.opencubicchunks.gradle.mcGitVersion")
     id("io.github.opencubicchunks.gradle.mixingen")
     id("io.github.opencubicchunks.gradle.dasm")
@@ -285,7 +285,7 @@ dependencies {
     debugCompile("org.lwjgl:lwjgl-vulkan:$lwjglVersion")
     debugRuntime("org.lwjgl:lwjgl::$lwjglNatives")
 
-    include(implementation("com.github.OpenCubicChunks:dasm:81e0a37")!!)
+    include(implementation("com.github.OpenCubicChunks:dasm:9709b98")!!)
     include(implementation("io.github.opencubicchunks:regionlib:0.63.0-SNAPSHOT")!!)
     include(implementation("org.spongepowered:noise:2.0.0-SNAPSHOT")!!)
 
